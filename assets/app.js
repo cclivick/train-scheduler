72ee5c3971e194994ad0bb59edd94051d661273a
cclivick <46388110+cclivick@users.noreply.github.com>
2019-05-22 17:46:10 -0500

    Bootstrap, Jquery, and Firebase linked into project - Firebase initialized in app.js

---
+++ assets/app.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+        var firebaseConfig = {
+            apiKey: "AIzaSyD8uTYdX4UrWzu89noTn1yDIuVY4vdVc70",
+            authDomain: "train-scheduler-c0a9c.firebaseapp.com",
+            databaseURL: "https://train-scheduler-c0a9c.firebaseio.com",
+            projectId: "train-scheduler-c0a9c",
+            storageBucket: "train-scheduler-c0a9c.appspot.com",
+            messagingSenderId: "230671423242",
+            appId: "1:230671423242:web:6389f52e86f6261a"
+        };
+
+        firebase.initializeApp(firebaseConfig);
+
+        
+
+    });
