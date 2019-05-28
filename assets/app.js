4899810e83238773c800cbc3c23f298d5ce5deeb
cclivick <46388110+cclivick@users.noreply.github.com>
2019-05-28 14:26:02 -0500

    Script updating firebase with new train info and displaying info on table - 'Next Arrival' and 'Minutes Away' not yet coded

--- assets/app.js
+++ assets/app.js
@@ -12,6 +12,59 @@ $(document).ready(function() {
 
         firebase.initializeApp(firebaseConfig);
 
-        
+        var trainData = firebase.database();
+
+        //Submit Button and remove default function
+        $("#submitBtn").on("click", function(event) {
+            event.preventDefault();
+            //Get user inputs
+                var trainName = $("#trainNameInput").val().trim();
+                var trainDest = $("#destinInput").val().trim();
+                var firstDepart = $("#timeInput").val().trim();
+                var frequency = $("#freqInput").val().trim();
+
+            //Store user data in temporary variable
+                var newTrain = {
+                    Name: trainName,
+                    Destination: trainDest,
+                    FirstTrain: firstDepart,
+                    Frequency: frequency
+                };
+
+            //Push newTrain to firebase
+                trainData.ref().push(newTrain);
+                    console.log(trainName);
+                    console.log(trainDest);
+                    console.log(firstDepart);
+                    console.log(frequency);
+
+            //Clear all textboxes for new newTrain
+                $("#trainNameInput").val("");
+                $("#destinInput").val("");
+                $("#timeInput").val("");
+                $("#freqInput").val("");
+            })
+
+        //Firebase event "child_added" to create 
+        trainData.ref().on("child_added", function(childSnapshot) {
+            console.log(childSnapshot.val());
+                var trainName = childSnapshot.val().Name;
+                var trainDest = childSnapshot.val().Destination;
+                var firstDepart = childSnapshot.val().FirstTrain;
+                var frequency = childSnapshot.val().Frequency;
+
+                //Calculate next arrival with moment.js
+
+                var newTrainRow = $("<tr>").append(
+                    $("<td>").text(trainName),
+                    $("<td>").text(trainDest),
+                    $("<td>").text(frequency),
+                    $("<td>").text(),
+                    $("<td>").text(),
+                )
+
+                $("#trainTable > tbody").append(newTrainRow);
+
+        })
 
     });
